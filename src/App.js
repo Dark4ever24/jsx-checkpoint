9125a419fb230b21f67add58c138bc287b4cdb0b
Dark4ever24 <samsopiol@tutanota.com>
2021-08-18 22:10:50 +0100

    jsx-checkpoint

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import image from './myimage.jpeg';
+import video from './myvideo.mp4';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+    <div style={{border:'solid 1px black', maxWidth: "100vw" }}>
+      <h1 className="title-red">JSX-checkpoint</h1>
+    <img className="image1" src={image} alt="imageInSrc" />
+    <img className="image2" src="portalinpublic.jpeg" alt="jsx" />
+    </div>
+    <video className="video1" width="750" height="500" controls >
+      <source src={video} type="video/mp4"/>
+     </video>
+  </div>
+  );
+}
+
+export default App;
